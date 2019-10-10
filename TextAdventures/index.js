eb80ce3d63d6d32af7677e7d89c8aba442c23f30
Seniru Pasan Indira <senirupasan@gmail.com>
2019-10-10 06:14:52 +0000

    Add more color effects and improvements
    - Changed the color of the description to red if player lose, else green
    
    - Changed boolean-like values inside quotations and made them valid
    booleans. This also reduced the length of some lines

--- TextAdventures/index.js
+++ TextAdventures/index.js
@@ -14,6 +14,12 @@ function getValues(n) {
     return choices;
 }
 
+/*Salif's coloring function*/
+function clr(text, color) {
+	var code = { red: 91, green: 92, blue: 34, cian: 96, yellow: 93 }[color];
+	if (code) return "\x1b[" + code + "m" + text + "\x1b[0m";
+}
+
 (async () => {
 
     while (true) {
@@ -24,8 +30,8 @@ function getValues(n) {
         choices: getValues(node),
         hint: '- Space to select. Return to submit'
     });
-    if (response.value.over == "true") {
-        console.log(response.value.description)
+    if (response.value.over) {
+        console.log(clr(response.value.description, response.value.win ? "green" : "red"));
         break;
     }
     node = response.value.answers;
