8a9bc7e56babde2a285b0fb0d8fe29bbfe642762
Salif Mehmed <mail@salif.eu>
2023-10-09 21:32:18 +0300

    Sort Hangman translation files

--- Hangman/index.js
+++ Hangman/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 
 const Colors = {
     red: 91,
--- Hangman/sort.js
+++ Hangman/sort.js
@@ -1,14 +1,25 @@
-var fs = require("fs")
-var language = "en-us";
+const fs = require("fs")
+var language = "en-us"
 
-if(process.argv.length > 2) {
-	language = process.argv[2];
+if (process.argv.length > 2) {
+    language = process.argv[2]
 }
 
-var db = require(`./words/${language}.json`);
-db.words = db.words.sort();
-fs.writeFile(`./words/${language}.json`, JSON.stringify(db, null, "\t"), function(err) {
-    if (err) {
-        console.log(err);
+const db = require(`./words/${language}.json`)
+const newWords = new Set()
+const testRegex = db.regexp != undefined
+const regexp = new RegExp(db.regexp)
+db.words.forEach(word => {
+    if (testRegex) {
+        if (!regexp.test(word)) {
+            console.log(word)
+        }
     }
-});
+    newWords.add(word)
+})
+db.words = Array.from(newWords).sort()
+fs.writeFile(`./words/${language}.json`, JSON.stringify(db, null, "\t"), function (err) {
+    if (err) {
+        console.log(err)
+    }
+})
