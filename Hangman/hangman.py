fc2a2fabe31fcb7d7ca00f307c8e2bac1b8cb336
Etienne Carpentier <128465927+Etiennecarpentier@users.noreply.github.com>
2025-06-02 17:09:13 +0200

    correction du hangman python

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -31,49 +31,19 @@ def showLetter(data):
 			wordl -= 1
 
 def randWord():
-	return words[randint(0, len(words))]
+	return words[randint(0, len(words)-1)]
 
 def initGame():
-	global word, wordl, obfWord, playedLetters
+	global word, wordl, obfWord, playedLetters, guessesRemaining
 	word = list(randWord())
 	wordl = len(word)
 	obfWord = ["_"]*wordl
+	playedLetters = []
+	guessesRemaining = 8
 	showLetter(word[0])
 	showLetter(word[-1])
 	playedLetters.append(word[0])
 	playedLetters.append(word[-1])
-
-def check(data):
-	global word, wordl, obfWord, playedLetters, guessesRemaining
-	if re.search("^.{1}$", data):
-		if data in playedLetters:
-			print(clr("\n " + db["played"], "red"))
-		else:
-			playedLetters.append(data)
-			if data in word:
-				showLetter(data)
-				if wordl < 1:
-					print(clr("\n " + " ".join(obfWord), "cian"))
-					print(clr("\n --------------------------------------------", "blue"))
-					print(clr("\n  " + db["you_won"] + "\n", "green"))
-					sys.exit()
-			else:
-				guessesRemaining -= 1
-			if guessesRemaining < 1:
-				print(clr("\n " + " ".join(obfWord) + "\n", "cian"))
-				print(clr("\n --------------------------------------------", "blue"))
-				print(clr("\n " + "".join(word), "cian"))
-				print(clr("\n " + db["you_lose"] + "\n", "red"))
-				sys.exit()
-	else:
-		print(clr("\n " + db["one_char"], "red"))
-	print(clr("\n " + (" ").join(obfWord) + "\n", "cian"))
-	print(clr(" " + str(guessesRemaining) + " " + db["remaining"], "green"))
-	print(clr(" " + db["letters"] + " " + (", ").join(playedLetters), "green"))
-	print(clr("\n --------------------------------------------\n", "blue"))
-	print(clr(" " + db["guess"] + " ", "yellow"), end='')
-	data = input()
-	check(data)
 
 def clr(text, color):
 	if color == "red":
@@ -87,13 +57,63 @@ def clr(text, color):
 	elif color == "yellow":
 		return "\x1b[93m" + text + "\x1b[0m"
 
+def check(data):
+	global word, wordl, obfWord, playedLetters, guessesRemaining
+	if re.search("^.{1}$", data):
+		if data in playedLetters:
+			print(clr("\n " + db["played"], "red"))
+		else:
+			playedLetters.append(data)
+			if data in word:
+				showLetter(data)
+				if wordl < 1:
+					return "won"
+			else:
+				guessesRemaining -= 1
+			if guessesRemaining < 1:
+				return "lost"
+	else:
+		print(clr("\n " + db["one_char"], "red"))
+
+	print(clr("\n " + (" ").join(obfWord) + "\n", "cian"))
+	print(clr(" " + str(guessesRemaining) + " " + db["remaining"], "green"))
+	print(clr(" " + db["letters"] + " " + (", ").join(playedLetters), "green"))
+	print(clr("\n --------------------------------------------\n", "blue"))
+	return "continue"
+
+def playGame():
+	initGame()
+	print(clr(" " + " ".join(obfWord) + "\n", "cian"))
+	while True:
+		print(clr(" " + db["guess"] + " ", "yellow"), end='')
+		data = input().strip().lower()
+		result = check(data)
+		if result == "won":
+			print(clr("\n " + " ".join(obfWord), "cian"))
+			print(clr("\n --------------------------------------------", "blue"))
+			print(clr("\n  " + db["you_won"] + "\n", "green"))
+			break
+		elif result == "lost":
+			print(clr("\n " + " ".join(obfWord) + "\n", "cian"))
+			print(clr("\n --------------------------------------------", "blue"))
+			print(clr("\n " + "".join(word), "cian"))
+			print(clr("\n " + db["you_lose"] + "\n", "red"))
+			break
+
 def main():
 	print(clr("     " + db["hangman"], "green"))
 	print(clr("\n --------------------------------------------\n", "blue"))
-	initGame()
-	print(clr(" " + " ".join(obfWord) + "\n", "cian"))
-	print(clr(" " + db["guess"] + " ", "yellow"), end='')
-	data = input()
-	check(data.strip().lower())
+	while True:
+		playGame()
+		print(clr(f"{db['play_again']} (R) / {db['quit']} (Q): ", "yellow"), end='')
+		choice = input().strip().lower()
+		if choice == 'r':
+			continue
+		elif choice == 'q':
+			print(clr(db["goodbye"], "green"))
+			break
+		else:
+			print(clr(db["invalid_choice"], "red"))
 
-main()
+if __name__ == "__main__":
+	main()
